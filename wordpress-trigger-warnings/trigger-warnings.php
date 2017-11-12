cb4d8f198050067076cc4a2186115187e9dfb989
Will Ellwood <fragmad@gmail.com>
2017-11-12 17:44:45 +0000

    Changed language to "Content Warning"
    This change also allows the short tag [content_warning] to be used
    instead of [trigger_warning], which remains as an alias.

--- wordpress-trigger-warnings/trigger-warnings.php
+++ wordpress-trigger-warnings/trigger-warnings.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * @package Trigger Warnings
+ * @package Content Warnings
  * @version 0.5
  */
 /*
-Plugin Name: Trigger Warnings
+Plugin Name: Content Warnings
 Plugin URI:  https://github.com/fragmad/trigger-warning-wordpress
-Description: A plugin to provide a Wordpress shortcode to mark material which may upset potential readers and provide them
-with the ability to choose if they read content or not.
+Description: A plugin to provide a Wordpress shortcode to mark material which may upset potential readers and provide them with the ability to choose if they read content or not.
 Author: Will Ellwood
 Version: 0.5
 Author URI: http://www.github.com/fragmad
@@ -20,14 +19,14 @@ function compose_warnings($type) {
     case 'triggering':
         return 'material';
     case 'abuse':
-        return 'material about abusive behaviour';
+        return 'abusive behaviour';
         break;
     case 'slurs':
-        return 'material involving ethnic or racist slurs';
+        return 'ethnic or racist slurs';
     case 'sexual_violence':
-        return 'material contaning references to sexual violence';
-    case 'physcal_violence':
-        return 'material contaning references to physical violence';
+        return 'sexual violence';
+    case 'physical_violence':
+        return 'physical violence';
     }
 
     return $warning_string;
@@ -36,18 +35,18 @@ function compose_warnings($type) {
 
 function tag_post() {
     $post_id = get_the_ID();
-    wp_set_post_tags($post_id, 'trigger-warning', true );
+    wp_set_post_tags($post_id, 'content-warning', true );
 }
 
-function trigger_warning_func( $atts) {
+function content_warning_func( $atts) {
     $a = shortcode_atts( array(
         'type' => 'triggering',
     ), $atts );
 
-    $trigger_types = array('triggering', 'abuse', 'sexual_violence', 'physical_violence', 'slurs');
+    $warning_types = array('triggering', 'abuse', 'sexual_violence', 'physical_violence', 'slurs');
 
 
-    if (in_array($a['type'], $trigger_types)) {
+    if (in_array($a['type'], $warning_types)) {
         $warnings = compose_warnings($a['type']);
     }
     else {
@@ -62,9 +61,23 @@ function trigger_warning_func( $atts) {
     }
 
     tag_post();
-    $warning_message = '<p><b>TRIGGER WARNING</b> This page contains ' . $warnings . ' which may be triggering for survivors.</p>';
+    $warning_message = '<p><b>CONTENT WARNING</b> This page contains ' . $warnings . ' which may be triggering for survivors.</p>';
     return $warning_message;
 }
 
-add_shortcode('trigger_warning', 'trigger_warning_func');
+add_shortcode('content_warning', 'content_warning_func');
+add_shortcode('trigger_warning', 'content_warning_func');
+
+// Add content warning quicktag to HTML editor
+function appthemes_add_contentwarning_quicktag() {
+    if (wp_script_is('quicktags')){
+?>
+    <script type="text/javascript">
+    QTags.addButton( 'trigger_warning', 'trigger_warning', '[trigger_warning]', '', 'tw', 'Content Warning', 201 );
+    QTags.addButton( 'content_warning', 'content_warning', '[content_warning]', '', 'cw', 'Content Warning', 201 );
+    </script>
+<?php
+    }
+}
+add_action( 'admin_print_footer_scripts', 'appthemes_add_contentwarning_quicktag' );
 ?>
